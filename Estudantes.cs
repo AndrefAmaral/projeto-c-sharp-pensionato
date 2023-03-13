2b87bded2e07b45b89a52db798c59c3f514ef957
Andre Felipe <andrefelipe.pi2@gmail.com>
2023-03-13 17:31:01 -0300

    Exercicio de vetores

---
+++ Estudantes.cs
@@ -0,0 +1,18 @@
+namespace Pensionato
+{
+    class Estudantes
+    {
+        public string Nome {get; set;}
+        public string Email {get; set;}
+
+        public Estudantes(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string  ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
